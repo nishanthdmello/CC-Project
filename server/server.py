d523fa8feb5aee4ac56c6d323433d4f9db6a217c
Digvijay Narayan <digvijaynarayan@Digvijays-Laptop.local>
2024-03-24 15:44:54 +0530

    migrated code from etcd to etcd3

--- server/server.py
+++ server/server.py
@@ -1,50 +1,48 @@
 from flask import Flask, request, jsonify
-from etcd import Client, EtcdKeyNotFound
-
+import etcd3
 
 app = Flask(__name__)
+etcd = etcd3.client(port=4001)
 
+# PUT operation
+@app.route('/put', methods=['POST'])
+def put():
+    data = request.get_json()
+    key = data['key']
+    value = data['value']
+    etcd.put(key, value)
+    return jsonify({'message': 'Key {} set to {}'.format(key, value)}), 200
 
-client = Client(host='localhost', port=5007)
-
-
-@app.route('/set', methods=['POST'])
-def set_key():
-    key = request.json.get('key')
-    value = request.json.get('value')
-    if not key or not value:
-        return "Key and value must be provided in JSON format.", 400
-
-    try:
-        client.write(key, value)
-        return f"Successfully set {key} to {value}"
-    except Exception as e:
-        return f"Error settin  {key}: {str(e)}", 500
-
-
+# GET operation
 @app.route('/get', methods=['GET'])
-def get_key():
+def get():
     key = request.args.get('key')
-    if not key:
-        return "Key must be provided as a query parameter.", 400
+    if key is None:
+        return jsonify({'error': 'Key parameter is required'}), 400
 
     try:
-        response = client.read(key)
-        return response.value
-    except EtcdKeyNotFound:
-        return f"Key {key} not found in etcd", 404
+        value_bytes = etcd.get(key)
+        print(value_bytes)
+        if value_bytes:
+            value_string = value_bytes[0].decode('utf-8')
+            return jsonify({'value': value_string}), 200
+        else:
+            return jsonify({'message': 'Key not found'}), 404
     except Exception as e:
-        return f"Error getting value for {key}: {str(e)}", 500
+        return jsonify({'error': str(e)}), 500
 
+# GET all keys
 @app.route('/getall', methods=['GET'])
 def get_all_keys():
     try:
-        response = client.read('/', recursive=True)
-        key_values = {node.key: node.value for node in response.leaves}
-        return jsonify(key_values)
+        value_list=[]
+        for key, value in etcd.get_all():
+            decoded_value=key.decode('utf-8')
+            value_list.append(decoded_value)
+        print(value_list)
+        return jsonify({"values":value_list}), 200
     except Exception as e:
         return f"Error getting all key-value pairs: {str(e)}", 500
-
 
 if __name__ == '__main__':
     app.run(debug=True)
